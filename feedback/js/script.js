6863868b3af07799b07872e4cf5cd7a66a3b818f
Anatol Bogdan <bogdan.anatol@gmail.com>
2015-03-26 20:51:54 -0700

    Форма отзыва: оживление числовых полей

---
+++ feedback/js/script.js
@@ -0,0 +1,40 @@
+(function() {
+
+var
+  form = document.querySelector('.response'),
+  travelTime = document.querySelector('.travel-time'),
+  adults = document.querySelector('.people-count'),
+  children = document.querySelector('.children-count'),
+  plus = document.querySelectorAll('.minus'),
+  minus = document.querySelectorAll('.plus'),
+  counter = document.querySelectorAll('.counter-input');
+
+  plus[0].addEventListener("click", function() {
+    travelTime.value++;
+  });
+
+  plus[1].addEventListener("click", function() {
+    adults.value++;
+  });
+
+  plus[2].addEventListener("click", function() {
+    children.value++;
+  });
+
+
+  minus[0].addEventListener("click", function() {
+    if (counter[0].value > 0)
+    travelTime.value--;
+  });
+
+  minus[1].addEventListener("click", function() {
+    if (counter[1].value > 0)
+    adults.value--;
+  });
+
+  minus[2].addEventListener("click", function() {
+    if (counter[2].value > 0)
+    children.value--;
+  });
+
+})();
