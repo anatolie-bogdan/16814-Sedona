b1eddb9870f7b80f553ca348864da910fc9107a3
Anatol Bogdan <bogdan.anatol@gmail.com>
2015-03-31 00:23:32 -0700

    финальная версия

--- js/script.js
+++ js/script.js
@@ -37,3 +37,36 @@ $("[href=#hotels]").click(function() {
     }, 1000);
 
 });
+
+
+// buttons counter
+
+(function() {
+
+var
+  form = document.querySelector('.sedonaHotels__book'),
+  adults = document.querySelector('.form__inputField--left'),
+  children = document.querySelector('.form__inputField--right'),
+  plus = document.querySelectorAll('.form__counterBtn--plus'),
+  minus = document.querySelectorAll('.form__counterBtn--minus'),
+  counter = document.querySelectorAll('.counter-input');
+
+  plus[0].addEventListener("click", function() {
+    adults.value++;
+  });
+
+  plus[1].addEventListener("click", function() {
+    children.value++;
+  });
+
+  minus[0].addEventListener("click", function() {
+    if (adults.value > 1)
+    adults.value--;
+  });
+
+  minus[1].addEventListener("click", function() {
+    if (children.value > 0)
+    children.value--;
+  });
+
+})();
