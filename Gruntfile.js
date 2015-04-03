72d3e652c544cbaeba13cffbd916f0b6e79f8287
Anatol Bogdan <bogdan.anatol@gmail.com>
2015-04-03 13:30:55 -0700

    таски grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,45 @@
 module.exports = function(grunt) {
 
+  require('load-grunt-tasks')(grunt);
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    // CSS Min
+      cssmin: {
+        options: {
+          shorthandCompacting: false,
+          roundingPrecision: -1,
+          report: 'gzip'
+        },
+        target: {
+          files: {
+            '/css/stylesheets/screen.min.css': ['/css/stylesheets/screen.css']
+          }
+        }
+      }
+
+    // Media
+      cmq: {
+        options: {
+          log: false
+        },
+        style: {
+          files: {
+            '/css/stylesheets/screen.css': ['/css/stylesheets/screen.css']
+          }
+        }
+      }
+
+
+    // Autoprefixer
+      autoprefixer: {
+        multiple_files: {
+          expand: true,
+          flatten: true,
+          src: '/css/stylesheets/*.css', // -> src/css/file1.css, src/css/file2.css 
+        }
+      }
 
     // Lint Spaces in code
     lintspaces: {
@@ -24,9 +62,38 @@ module.exports = function(grunt) {
         }
       }
     }
+
+    sass: {                              // Task 
+      dist: {                            // Target 
+        options: {                       // Target options 
+          style: 'expanded'
+        },
+        files: {                         // Dictionary of files 
+          'css/sass/screen.css': ['css/stylesheets/screen.scss']       // 'destination': 'source'
+        }
+      }
+    }
+
+    //CSS Comb
+    csscomb: {
+      style: {
+        expand: true,
+        src: ["css/sass/**/*.scss"]
+      }
+    }
+
+    //Image min
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: {[
+          expand: true,
+          src: ["img/**/*.{png,jpg,svg,gif}"]
+          ]}
+      }
+    }
+
   });
-
-  grunt.loadNpmTasks('grunt-lintspaces');
-
-  grunt.registerTask('lint', ['lintspaces']);
 };
